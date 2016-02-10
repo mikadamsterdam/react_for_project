3135be43edabd495029da0062778f60b5056bbbf
rorykoehein <rory@intelworks.com>
2016-02-10 21:21:25 +0100

    Hello World

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to React!</h1>
+                <p>A Javascript library for building awesome user interfaces.</p>
+                <a href="https://facebook.github.io/react/">Read more about React</a>
+            </div>
+        );
+    }
+}
+
+export default App;
