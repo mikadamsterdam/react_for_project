db0d580afb6ff9ae069b4ffaf2d2adf08cef555c
rorykoehein <rory@intelworks.com>
2016-02-10 21:42:08 +0100

    2.5 - 2.7 Counter constructor and initial state

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 class Counter extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            count: 0
+        };
+    }
+
     render() {
         return (
             <div>
-                <h2>0</h2>
+                <h2>{this.state.count}</h2>
                 <button>+1</button>
             </div>
         );
