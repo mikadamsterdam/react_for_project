79a511b5dc0466982de44f72267007bec60c29ce
Chiel Hackman <chielhackman@gmail.com>
2016-02-23 14:15:33 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,14 @@
 import React from 'react';
+import ScoreBoard from './Scoreboard';
 
 class App extends React.Component {
-
-  constructor(){
-    super();
-    this.state = {
-      TaskTitle: "Here comes the title of the task.",
-      Description: "Here comes the desciption of the task."
-    };
-  }
-
     render() {
         return (
-          <div>
-          <h1>ProjectName</h1>
-          <hr />
-          <table>
-            <tbody>
-            <button>New Task</button>
-              <h3>{this.state.TaskTitle}</h3>
-                <p>{this.state.Description}</p>
-              </tbody>
-          </table>
-          <table>
-            <tbody>
-              <h3>{this.state.TaskTitle}</h3>
-                <p>{this.state.Description}</p>
-              </tbody>
-          </table>
-          <table>
-            <tbody>
-              <h3>{this.state.TaskTitle}</h3>
-                <p>{this.state.Description}</p>
-              </tbody>
-          </table>
-          <table>
-            <tbody>
-              <h3>{this.state.TaskTitle}</h3>
-                <p>{this.state.Description}</p>
-              </tbody>
-          </table>
-          <table>
-            <tbody>
-              <h3>{this.state.TaskTitle}</h3>
-                <p>{this.state.Description}</p>
-              </tbody>
-          </table>
-          </div>
+            <div>
+                <h1>ProjectName</h1>
+                      <button onClick={this.create}>New task</button>
+                <ScoreBoard />
+            </div>
         );
     }
 }
